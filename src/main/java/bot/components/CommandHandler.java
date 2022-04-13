d17065b7fb308fad184ba06cacbaf5d654186655
Maheesha <maheeshasithum@gmail.com>
2022-04-13 19:56:01 +0530

    bug fixes

--- src/main/java/bot/components/CommandHandler.java
+++ src/main/java/bot/components/CommandHandler.java
@@ -3,7 +3,6 @@ package bot.components;
 import bot.UntitledBot;
 import bot.components.annotations.ArgType;
 import bot.components.annotations.CommandArg;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +11,6 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.ListIterator;
 
 public class CommandHandler extends ListenerAdapter {
     @Override
@@ -22,7 +20,7 @@ public class CommandHandler extends ListenerAdapter {
 
         String[] messageContents = event.getMessage().getContentRaw().substring(1).split("\\s");
         String command = messageContents[0];
-        String[] commandArgContents = Arrays.copyOfRange(messageContents,1, messageContents.length-1);
+        var commandArgContents = new ArrayList<>(Arrays.asList(Arrays.copyOfRange(messageContents, 1, messageContents.length)));
 
         if(!UntitledBot.CommandMap.containsKey(command)) return;
 
@@ -33,24 +31,22 @@ public class CommandHandler extends ListenerAdapter {
         commandObject.run(args, event.getMessage(), event.getGuild());
     }
 
-    public HashMap<String,String> parseArgument(String[] contents, ChatCommand commandObject){
+    public HashMap<String,String> parseArgument(ArrayList<String> contents, ChatCommand commandObject){
         var fieldList = new ArrayList<>(Arrays.asList(commandObject.getClass().getDeclaredFields()));
         fieldList.removeIf(field -> !field.isAnnotationPresent(CommandArg.class));
-        if(contents.length < fieldList.size()) return null;
+        if(contents.size() < fieldList.size()) return null;
 
         Field[] orderedList = new Field[fieldList.size()];
-        for(Field field : fieldList){
-            System.out.println(field.getName());
-            orderedList[field.getAnnotation(CommandArg.class).index()] = field;
-        }
+        for(Field field : fieldList){ orderedList[field.getAnnotation(CommandArg.class).index()] = field; }
 
-        HashMap<String,String> hashMap = new HashMap<String,String>();
-        for(int i = 0; i < orderedList.length; i++){
-            if(orderedList[i].getAnnotation(CommandArg.class).type() == ArgType.STRING_COALESCING){
-                hashMap.put(orderedList[i].getName(), Arrays.toString(contents));
+        HashMap<String,String> hashMap = new HashMap<>();
+        for(Field field : orderedList){
+            if(field.getAnnotation(CommandArg.class).type() == ArgType.STRING_COALESCING){
+                hashMap.put(field.getName(), String.join(" ", contents));
                 break;
             }else {
-                hashMap.put(orderedList[i].getName(), contents[i]);
+                hashMap.put(field.getName(), contents.get(0));
+                contents.remove(0);
             }
         }
         return hashMap;
