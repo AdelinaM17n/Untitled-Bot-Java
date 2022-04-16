51c469ff74777e215f797a191af4a5951b8b9761
Maheesha <maheeshasithum@gmail.com>
2022-04-16 22:18:19 +0530

    removing the unban for now

--- src/main/java/bot/UntitledBot.java
+++ src/main/java/bot/UntitledBot.java
@@ -1,7 +1,6 @@
 package bot;
 
 import bot.components.BanCommand;
-import bot.components.UnbanCommand;
 import bot.lib.commandhandler.ChatCommand;
 import bot.lib.commandhandler.CommandHandler;
 import net.dv8tion.jda.api.JDABuilder;
@@ -9,13 +8,14 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class UntitledBot {
     public static HashMap<String, ChatCommand> CommandMap = new HashMap<>();
     public static String prefix = "!";
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws LoginException, NoSuchMethodException, InvocationTargetException, IllegalAccessException{
         JDABuilder jda = JDABuilder.createDefault(args[0]);
         jda.setStatus(OnlineStatus.DO_NOT_DISTURB);
         jda.enableIntents(GatewayIntent.GUILD_MESSAGES);
@@ -25,8 +25,7 @@ public class UntitledBot {
         );
 
         addCommands(
-                new BanCommand(),
-                new UnbanCommand()
+                new BanCommand()
         );
 
         jda.build();
