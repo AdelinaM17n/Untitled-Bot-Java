068dd01de857bf329a01d1a2c109b1776ec0326c
Maheesha <maheeshasithum@gmail.com>
2022-04-16 22:03:15 +0530

    reflection, and reflecting on my life decisions

--- src/main/java/bot/lib/commandhandler/ChatCommand.java
+++ src/main/java/bot/lib/commandhandler/ChatCommand.java
@@ -1,34 +1,57 @@
 package bot.lib.commandhandler;
 
+import bot.lib.commandhandler.annotation.ArgInnerClass;
 import bot.lib.commandhandler.annotation.CommandArg;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Message;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public abstract class ChatCommand {
     private final String name;
     private final int nonOptionalArgCount;
     private final Field[] orderedFieldList;
+    private final Method executionMethod;
+    private final Class<?> argsClass;
+    private final Class<?>[] argsFieldTypeList;
 
     public ChatCommand(String name){
         this.name = name;
-        nonOptionalArgCount = setNonOptionalArgCount();
-        orderedFieldList = setOrderedFieldList();
+        argsClass = setArgsObject();
+        orderedFieldList = setOrderedFieldList(argsClass);
+        argsFieldTypeList = setArgsFieldTypeList(orderedFieldList);
+        nonOptionalArgCount = setNonOptionalArgCount(argsClass);
+        executionMethod = setExecutionMethod();
     }
 
-    public abstract void run(HashMap<String,Object> args, Message message, Guild guild);
+    //public abstract void run(HashMap<String,Object> args, Message message, Guild guild);
+    private Class<?>[] setArgsFieldTypeList(Field[] orderedFieldList){
+        var filedTypeArray = new Class<?>[orderedFieldList.length+1];
+        filedTypeArray[0] = this.getClass();
+        for(int i =1; i < orderedFieldList.length; i++){
+            filedTypeArray[i] = orderedFieldList[i-1].getType();
+        }
+        return filedTypeArray;
+    }
 
-    private int setNonOptionalArgCount(){
-        var fieldList =  new ArrayList<>(Arrays.stream(this.getClass().getDeclaredFields()).toList());
+    private Class<?> setArgsObject(){
+        var innerclass = Arrays.stream(this.getClass().getDeclaredClasses()).filter(aClass -> aClass.isAnnotationPresent(ArgInnerClass.class)).findFirst();
+        return innerclass.orElse(null);
+    }
+
+    private Method setExecutionMethod(){
+        var method = Arrays.stream(this.getClass().getDeclaredMethods()).filter(method1 -> method1.getName().equals("run")).findFirst();
+        return method.orElse(null);
+    }
+
+    private int setNonOptionalArgCount(Class<?> clazz){
+        var fieldList =  new ArrayList<>(Arrays.stream(clazz.getDeclaredFields()).toList());
         fieldList.removeIf(field -> !field.isAnnotationPresent(CommandArg.class) || (field.isAnnotationPresent(CommandArg.class) && field.getAnnotation(CommandArg.class).optional()));
         return fieldList.size();
     }
-    private Field[] setOrderedFieldList(){
-        var fieldList = new ArrayList<>(Arrays.asList(this.getClass().getDeclaredFields()));
+    private Field[] setOrderedFieldList(Class<?> clazz){
+        var fieldList = new ArrayList<>(Arrays.asList(clazz.getDeclaredFields()));
         fieldList.removeIf(field -> !field.isAnnotationPresent(CommandArg.class));
         Field[] orderedList = new Field[fieldList.size()];
         for(Field field : fieldList){ orderedList[field.getAnnotation(CommandArg.class).index()] = field; }
@@ -45,6 +68,18 @@ public abstract class ChatCommand {
 
     public Field[] getOrderedFieldList(){
         return Arrays.copyOf(orderedFieldList, orderedFieldList.length);
+    }
+
+    public Class<?>[] getArgsFieldTypeList(){
+        return Arrays.copyOf(argsFieldTypeList,argsFieldTypeList.length);
+    }
+
+    public Method getExecutionMethod(){
+        return executionMethod;
+    }
+
+    public Class<?> getArgsClass(){
+        return argsClass;
     }
 
     public String getName(){
--- src/main/java/bot/lib/commandhandler/CommandHandler.java
+++ src/main/java/bot/lib/commandhandler/CommandHandler.java
@@ -4,16 +4,15 @@ import bot.UntitledBot;
 import bot.lib.commandhandler.annotation.ArgType;
 import bot.lib.commandhandler.annotation.CommandArg;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class CommandHandler extends ListenerAdapter {
     @Override
@@ -37,35 +36,55 @@ public class CommandHandler extends ListenerAdapter {
             return;
         }
 
-        commandObject.run(args, event.getMessage(), event.getGuild());
+        try{
+            var innerClass = commandObject.getArgsClass();
+            var constructor = innerClass.getDeclaredConstructor(commandObject.getArgsFieldTypeList());
+
+            var argsInstance = innerClass.cast(constructor.newInstance(args));
+            commandObject.getExecutionMethod().invoke(commandObject,argsInstance,event.getMessage(),event.getGuild());
+
+        }catch(NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e){
+            e.printStackTrace();
+        }
+
+        //commandObject.run(args, event.getMessage(), event.getGuild());
     }
 
-    public HashMap<String,Object> parseArgument(ArrayList<String> contents, ChatCommand commandObject, JDA apiWrapper){
+    public Object[] parseArgument(ArrayList<String> contents, ChatCommand commandObject, JDA apiWrapper){
         if(contents.size() < commandObject.getNonOptionalArgCount()) return null;
 
         Field[] orderedList = commandObject.getOrderedFieldList();
-        HashMap<String,Object> hashMap = new HashMap<>();
-
+        Object[] listObjects = new Object[orderedList.length];
+        //HashMap<String,Object> hashMap = new HashMap<>();
+        boolean hasMetCoalesc = false;
         for(int i =0; i < orderedList.length; i++){
-            if(orderedList[i].getAnnotation(CommandArg.class).type() == ArgType.STRING_COALESCING){
-                hashMap.put(orderedList[i].getName(), String.join(" ", contents));
+            if(hasMetCoalesc){
+                listObjects[i] = null;
+            }else if(orderedList[i].getAnnotation(CommandArg.class).type() == ArgType.STRING_COALESCING){
+                listObjects[i] =  String.join(" ", contents);
                 orderedList[i] = null;
-                break;
+                hasMetCoalesc = true;
             }else {
                 var orderedListType = orderedList[i].getType();
                 if(orderedListType == String.class){
-                    hashMap.put(orderedList[i].getName(), contents.get(0));
+                    listObjects[i] = contents.get(0);
+                    orderedList[i] = null;
                 }else if (orderedListType == User.class){
-                    if(contents.get(0).startsWith("<@"))
-                        hashMap.put(orderedList[i].getName(),apiWrapper.retrieveUserById(contents.get(0).substring(3,21)));
-                    else
-                        hashMap.put(orderedList[i].getName(),apiWrapper.retrieveUserById(contents.get(0)));
+                    if(contents.get(0).startsWith("<@")){
+                        listObjects[i] = apiWrapper.retrieveUserById(contents.get(0).substring(3, 20));
+                        orderedList[i] = null;
+                    }else{
+                        listObjects[i] = apiWrapper.retrieveUserById(contents.get(0));
+                        orderedList[i] = null;
+                    }
+                }else {
+                    listObjects[i] = null;
                 }
                 contents.remove(0);
                 orderedList[i] = null;
             }
         }
 
-        return Arrays.stream(orderedList).anyMatch(field -> field != null && !field.getAnnotation(CommandArg.class).optional()) ? null : hashMap;
+        return Arrays.stream(orderedList).anyMatch(field -> field != null && !field.getAnnotation(CommandArg.class).optional()) ? null : listObjects;
     }
 }
